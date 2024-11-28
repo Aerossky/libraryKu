cc19839a02da48eb1baddeaa93689fb857c08716
Aerossky <92148435+Aerossky@users.noreply.github.com>
2024-11-29 02:00:30 +0700

    darkmode class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -20,5 +21,4 @@ export default {
         },
     },
     plugins: [],
-    darkMode: false,
 };
